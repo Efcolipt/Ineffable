c0c2f42a6e2ca591fba8bed1b9e3c4a5355869ef
Ivan <efcolipt@gmail.com>
2023-01-27 11:55:38 +0300

    update observer

--- src/plugins/observer.ts
+++ src/plugins/observer.ts
@@ -1,46 +1,23 @@
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.directive('observer', {
-    mounted(el, bind) {
-      if ('IntersectionObserver' in window) {
-        const observer = new IntersectionObserver(
-          (entries, observer) => {
-            const item = entries[0]
+    mounted(el, binding) {
+      const observer = new IntersectionObserver(
+        (entries, observer) => {
+          const item = entries[0]
 
-            if (item.isIntersecting) {
-              observer.unobserve(item.target)
-              bind.value()
-            }
-          },
-          {
-            root: null,
-            rootMargin: '0px',
-            threshold: 0,
+          if (item.isIntersecting) {
+            binding.value()
+            observer.unobserve(item.target)
           }
-        )
-
-        observer.observe(el)
-      } else {
-        const inViewport = (el: HTMLElement) => {
-          const elementBoundingClientRect = el.getBoundingClientRect()
-          const offset = 50
-
-          if (elementBoundingClientRect) {
-            return (
-              elementBoundingClientRect.top - offset >= 0 &&
-              elementBoundingClientRect.bottom > 0 &&
-              elementBoundingClientRect.bottom + offset <= window.innerHeight
-            )
-          } else {
-            return false
-          }
+        },
+        {
+          root: null,
+          rootMargin: '0px',
+          threshold: 0,
         }
+      )
 
-        document.onscroll = () => {
-          if (inViewport(el)) {
-            bind.value()
-          }
-        }
-      }
+      observer.observe(el)
     },
   })
 })
