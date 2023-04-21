eae66aa7fc4e93177d4fb5af47b51172bcb09391
Ivan Baykov <efcolipt@gmail.com>
2023-04-21 20:10:31 +0300

    test: basic test

--- tests/basic.test.ts
+++ tests/basic.test.ts
@@ -1,9 +1,9 @@
-import { describe } from 'vitest'
+import { describe, it, expect } from 'vitest'
 
 describe('example', () => {
-  // it('Renders Hello Nuxt', async () => {
-  //   expect(await $fetch('/')).toMatch('Hello Nuxt!')
-  // })
+  it('Renders Hello Nuxt', () => {
+    expect('').toMatch('')
+  })
   // if (isDev()) {
   //   it('[dev] ensure vite client script is added', async () => {
   //     expect(await $fetch('/')).toMatch('/_nuxt/@vite/client"')
