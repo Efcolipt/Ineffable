a90e8fbc2d17d7d11631716427ae079b8858e075
Ivan <efcolipt@gmail.com>
2023-04-23 16:34:05 +0300

    chore: add slider on main page

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -8,31 +8,16 @@ const { data } = useAsyncData(
 <template>
   <div>
     <Swiper
-      :height="300"
       :modules="[SwiperAutoplay, SwiperEffectCreative]"
       :slides-per-view="1"
       :loop="true"
-      :effect="'creative'"
       :autoplay="{
         delay: 8000,
         disableOnInteraction: true,
       }"
-      :creative-effect="{
-        prev: {
-          shadow: false,
-          translate: ['-20%', 0, -1],
-        },
-        next: {
-          translate: ['100%', 0, 0],
-        },
-      }"
     >
-      <SwiperSlide
-        v-for="item in data?.films"
-        :key="item.filmId"
-        class="cursor-pointer"
-      >
-        <!-- <img class="rounded-xl" :src="item.posterUrl" /> -->
+      <SwiperSlide v-for="item in data?.films" :key="item.filmId">
+        <img class="rounded-xl" :src="item.posterUrl" />
         {{ item.nameRu }}
       </SwiperSlide>
 
@@ -42,18 +27,11 @@ const { data } = useAsyncData(
 </template>
 
 <style scoped>
-:deep(.swiper-slide) {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 18px;
-  height: 20vh;
-  font-size: 4rem;
-  font-weight: bold;
-  font-family: 'Roboto', sans-serif;
-}
 :deep(.swiper-wrapper) {
-  min-width: 100vh;
-  width: 100vh;
+  display: flex;
+}
+
+:deep(.swiper-slide) {
+  flex: 0 0 100%;
 }
 </style>
